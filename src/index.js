09fc62f58175b864eccf5695e5fda0d2e2c15d04
Noopur Pachori <npachori@fanatics.com>
2021-06-20 17:29:40 +0530

    dded comment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    state = {lat: null, errorMessage:''}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    componentDidMount() {
+        console.log("My component was rendered to the screen");
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),
+            err => {
+                this.setState({errorMessage:err.message})
+            } 
+        );
+    }
+
+    componentDidUpdate() {
+        //Comp update
+        console.log("My component was updated - it rerendered");
+    }
+
+    renderContent() {
+        if(this.state.errorMessage && !this.state.lat)
+            return <div>Error: {this.state.errorMessage}</div>
+        
+        if(!this.state.errorMessage && this.state.lat)
+            return <SeasonDisplay lat={this.state.lat}/>
+
+        return <Spinner message="Please accept message request"/> 
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
+        
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector("#root"))
